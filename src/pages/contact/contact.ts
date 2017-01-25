efb68ebcb2b7819f2adb1e9bc12013ba4ea4d209
Kevin Liozon <kevinliozonpro@gmail.com>
2017-01-25 17:42:53 +0000

    First run on Android device, set a higher timeout, Creation of systems and inApp links, cordova plugins: geolocation and inAppBrowser added, Geolocation initialised on platform ready to allow using current position

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Platform } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -8,8 +8,50 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public platform: Platform) {
+    this.platform = platform;
+  }
 
+  initGeoloc(){
+    this.platform.ready().then(() => {
+      let locationOptions = {timeout: 70000, enableHighAccuracy: true};
+
+        navigator.geolocation.getCurrentPosition(
+
+            (position) => {
+                console.log(position);
+            },
+
+            (error) => {
+                console.log(error);
+            }, //locationOptions
+
+        );
+
+        navigator.geolocation.watchPosition(
+
+            (position) => {
+                console.log(position);
+            },
+
+            (error) => {
+                console.log(error);
+            }
+
+        );
+    });
+  }
+
+  systemBrowserLink(){
+    window.open("https://citymapper.com/go/c6a384", "_system");
+  }
+
+  inAppLink(){
+    window.open("https://citymapper.com/go/c6a384", "_blank");
+  }
+
+  inAppLinkNoUrl(){
+    window.open("https://citymapper.com/go/c6a384", "_blank", "location=no");
   }
 
 }
